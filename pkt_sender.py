22237a3f186614449481d0c842d0e7d71096fa0b
MohammadYasinKarbasian <m.y.karbasian1@gmail.com>
2022-06-03 18:16:23 +0000

    pkt sender optimized for tcp syn

--- pkt_sender.py
+++ pkt_sender.py
@@ -1,11 +1,14 @@
 from socket import *
 from binascii import unhexlify
-message = input('What is your packet content? ')
-#pkt = " ".join(message[i:i+2] for i in range(0,len(message),2))
-interface = input('Which interface do you want to use? ')
-pkt = unhexlify(message)
-s = socket(AF_PACKET, SOCK_RAW)
-s.bind((interface, 0))
-print(pkt)
+def ether_sender(message,interface):
+    #pkt = " ".join(message[i:i+2] for i in range(0,len(message),2))
+    pkt = unhexlify(message)
+    s = socket(AF_PACKET, SOCK_RAW)
+    s.bind((interface, 0))
+    s.send(pkt)
+    s.close()
+    print(f'Send {int(len(message)/2)}-byte packet on {interface}')
 
-s.send(pkt)
+# message = input('What is your packet content? ')
+# interface = input('Which interface do you want to use? ')   
+# ether_sender(message,interface) 
