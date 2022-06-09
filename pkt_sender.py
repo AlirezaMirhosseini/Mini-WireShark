113dbae309dc931684d57a1de23b5b5a4363c3d4
AlirezaMirhosseini <shapourdisco78@gmail.com>
2022-06-09 18:51:03 +0430

    test new mac address

--- pkt_sender.py
+++ pkt_sender.py
@@ -1,6 +1,6 @@
 from socket import *
 from binascii import unhexlify
-def ether_sender(message,interface):
+def ether_sender(message, interface):
     #pkt = " ".join(message[i:i+2] for i in range(0,len(message),2))
     pkt = unhexlify(message)
     s = socket(AF_PACKET, SOCK_RAW)
@@ -10,5 +10,5 @@ def ether_sender(message,interface):
     print(f'Send {int(len(message)/2)}-byte packet on {interface}')
 if __name__ == "__main__":
     message = input('What is your packet content? ')
-    interface = input('Which interface do you want to use? ')   
-    ether_sender(message,interface) 
+    interface = input('Which interface do you want to use? ')
+    ether_sender(message,interface)
