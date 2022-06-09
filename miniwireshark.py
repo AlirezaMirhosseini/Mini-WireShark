ffa1cdaa5e07b28443771e37047138da70faf582
AlirezaMirhosseini <shapourdisco78@gmail.com>
2022-06-09 17:32:30 +0430

    add tcp segment part

--- miniwireshark.py
+++ miniwireshark.py
@@ -15,9 +15,14 @@ def ether(data):
 def ipv4_address(address):
     return '.'.join(map(str, address))
 
-def ipv4_packet(data):
+def ip(data):
     version_internet_header_length = data[0]
     version = version_internet_header_length >> 4
     internet_header_length = (version_internet_header_length & 15) * 4
     ttl, proto, src, target = unpack('! 8x B B 2x 4s 4s', data[:20])
     return version, internet_header_length, ttl, proto, ipv4_address(src), ipv4_address(target), data[internet_header_length:]
+    
+def tcp(data):
+    src_port, dest_port, sequence, ack, offset_reserved_flags = unpack('! H H L L H', data[:14])
+    offset = (offset_reserved_flags >> 12) * 4
+    return (src_port, dest_port, sequence, ack, data[offset:])
